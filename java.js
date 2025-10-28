4641237de07da978b202cfeb85df59a6979d68f2
noiranova <roinajandayan02@gmail.com>
2025-10-29 00:49:22 +0800

    Update java.js

---
+++ java.js
@@ -0,0 +1,5 @@
+console.log(`Hello roi`);console.log("I love dogs");
+
+window.alert(`Alert!`);
+document.getElementById("myh1").textContent = `hello`;
+document.getElementById("P").textContent = `WROINGS`;
